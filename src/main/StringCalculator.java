71874688cb30c9f00401ab40405233c53c88bdf4
KaranChauhan1907 <18bce040@nirmauni.ac.in>
2021-08-27 02:55:15 +0530

    Refactor

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -3,12 +3,17 @@ package main;
 public class StringCalculator {
 
 	public int Add(String numbers) {
+		
 		if (numbers.isEmpty()) {
 			return 0;
 		}
 		else {
-			return Integer.parseInt(numbers);
+			return toInt(numbers);
 		}
+	}
+	
+	private int toInt(String numbers) {
+		return Integer.parseInt(numbers);
 	}
 	
 }
